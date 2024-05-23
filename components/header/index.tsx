4f2d422065ea0c2785af3ddee6539edfb4852594
Misy <78818724+zrfli@users.noreply.github.com>
2024-05-23 21:47:48 +0300

    Update index.tsx

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -10,9 +10,9 @@ export default function Header(){
                         <span className="text-purple-400 text-[10px] font-semibold align-top">Scrapper Beta</span>
                     </div>
                 </Link>
-                <div className="flex md:order-2 space-x-3 md:space-x-5 rtl:space-x-reverse items-center">
+                <div className="flex font-medium md:order-2 space-x-3 md:space-x-5 rtl:space-x-reverse items-center">
                     <Link href="mailto:misy@misy.dev" className="block py-2 px-3 text-[#888888] rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-[#888888]/70 text-sm dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</Link>
-                    <Link href="/login" className="text-sm text-white bg-black hover:bg-gray-900 border border-gray-800 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center">Log In</Link>
+                    <Link href="/login" className="text-sm text-white bg-black hover:bg-gray-900 border border-gray-800 focus:outline-none rounded-lg text-sm px-4 py-2 text-center">Log In</Link>
                 </div>
             </div>
         </nav>
